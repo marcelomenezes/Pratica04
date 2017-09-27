8ca28404117ff3f8c54f413deb5ea15f2bf18b4c
marcelomenezes <marcelohfms@gmail.com>
2017-09-27 09:07:26 -0300

    Layout pronto

--- app/src/main/java/ifpe/android/pratica04/MainActivity.java
+++ app/src/main/java/ifpe/android/pratica04/MainActivity.java
@@ -1,7 +1,12 @@
 package ifpe.android.pratica04;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonInsertClick(View view){
+
+        String name = ((EditText) findViewById(R.id.edit_name)).getText().toString();
+        String grade = ((EditText) findViewById(R.id.edit_grade)).getText().toString();
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit(); editor.putString(name, grade);
+        editor.apply();
+        Toast.makeText(this, "Salvo: " + name, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
